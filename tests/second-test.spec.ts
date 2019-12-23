b9289e63d13a93f302e88b5ca4cfd7352b513457
PolletMohsen <50701819+PolletMohsen@users.noreply.github.com>
2019-12-23 12:13:13 +0200

    Update second-test.spec.ts

--- tests/second-test.spec.ts
+++ tests/second-test.spec.ts
@@ -12,7 +12,7 @@
 
     test('login to app', async t => {
       await t.navigateTo('https://mail.google.com/');
-      await gmailmodel.gmailLogin('pollet.mohsen@crowdanalyzer.com','Abcd@123lyly');
+      await gmailmodel.gmailLogin('pollet.mohsen@crowdanalyzer.com','*********');
       await t.wait(9000);
       // await gmailmodel.getMailSubject();
        });
